530b99b9539e5eb21cb39cf70176f7eee3fdda54
med-more <166568621+med-more@users.noreply.github.com>
2025-02-04 11:27:48 +0000

    add animation with framer motion to the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,14 @@
 import {HERO} from "../constants"
 import carlImg from "../assets/medport.jpeg"
+import { motion } from "framer-motion"
 const Hero = () => {
   return (
     <section className="flex min-h-screen flex-wrap items-center">
-        <div className="w-full md:w-1/2">
+        <motion.div
+        initial = {{opacity:0, x:-50}}
+        animate = {{opacity: 1, x:0}}
+        transition={{duration:1}}
+         className="w-full md:w-1/2">
             <h2 className="my-8 p-2 text-4xl font-bold md:text-5xl lg:text[7rem]">
                 {HERO.name}
             </h2>
@@ -13,12 +18,20 @@ const Hero = () => {
             <p className="mb-8 p-2 text-xl">
                 {HERO.description}
             </p>
-        </div>
-        <div className="w-full mt-8 md:w-1/2 lg:p-8">
+        </motion.div>
+        <motion.div 
+        initial ={{opacity: 0, scale: 0.8}}
+        animate ={{opacity: 1, scale:1}}
+        transition={{duration:1}}
+        className="w-full mt-8 md:w-1/2 lg:p-8">
             <div className="flex justify-center">
-                <img src={carlImg} width={550} height={550} alt="med image" className="rounded-3xl" />
+                <motion.img
+                initial= {{opacity:0}}
+                animate={{opacity:1}}
+                transition={{duration:1}}
+                 src={carlImg} width={550} height={550} alt="med image" className="rounded-3xl" />
             </div>
-        </div>
+        </motion.div>
     </section>
   )
 }
